3fd90a7a372707b20c8ce54ce803a6c01bf084ed
marcowang01 <marcowang01@gmail.com>
2023-11-10 20:42:39 -0600

    nav bar styles baseline

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,8 +3,23 @@ import styles from './navbar.module.css'
 export default function Navbar() {
   return (
     <div className={styles.container}>
-      <div>
-        nav bar
+      <div className={styles.navbarContainer}>
+        <div className={styles.navbarLeftContainer}>
+          <div>
+            Marco Wang
+          </div>
+          <div>
+            Breadcrumbs
+          </div>
+        </div>
+        <div className={styles.navbarRightContainer}>
+          <div>
+            12:00 PM
+          </div>
+          <div>
+            Listening to: Hans Zimmer
+          </div>
+        </div>
       </div>
     </div>
   )
