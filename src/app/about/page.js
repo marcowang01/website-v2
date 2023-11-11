2a009664276128f4d15cf5addd484703ebeb487e
marcowang01 <marcowang01@gmail.com>
2023-11-11 00:30:55 -0600

    about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,7 +4,15 @@ import styles from './page.module.css'
 export default function About() {
   return (
     <main className={styles.main}>
-      about
+      <div className={styles.title}>
+        About
+      </div>
+      <div className={styles.paragraph}>
+        Hello! My name is Marco, and I am currently studying computer science and physics at Northwestern University.
+      </div>
+      <div>
+        Here is some cool bean.
+      </div>
     </main>
   )
 }
