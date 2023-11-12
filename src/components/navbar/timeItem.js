1931e6e8f81b351bd09722b4f4588adbaa5a6c30
marcowang01 <marcowang01@gmail.com>
2023-11-11 18:54:58 -0600

    fix date

--- src/components/navbar/timeItem.js
+++ src/components/navbar/timeItem.js
@@ -4,13 +4,19 @@ import { useState, useEffect } from 'react';
 import styles from './navbar.module.css'
 export default function TimeItem() {
   // useState hook to store the current time
-  const [currentTime, setCurrentTime] = useState(null);
+  const [currentTime, setCurrentTime] = useState('');
 
   // Function to update the current time
   const updateTime = () => {
     // Get the current time in Chicago using the 'America/Chicago' IANA timezone string
     const now = new Date().toLocaleString('en-US', { timeZone: 'America/Chicago' });
-    setCurrentTime(new Date(now));
+    const newDate = new Date(now);
+    const timeString = newDate.toLocaleTimeString('en-US', {
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true,
+    })
+    setCurrentTime(timeString.toUpperCase());
   };
 
   // useEffect hook to set the interval for updating the time
@@ -25,11 +31,7 @@ export default function TimeItem() {
 
   return (
     <div className={`${styles.navbarItems} ${styles.hideOnMobile}  ${styles.tight}`}>
-      {currentTime.toLocaleTimeString('en-US', {
-        hour: 'numeric',
-        minute: '2-digit',
-        hour12: true,
-      }).toUpperCase()}
+      {currentTime}
     </div>
   )
 }
