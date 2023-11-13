949915c8bba1645543ce659944939b3afdb871fa
marcowang01 <marcowang01@gmail.com>
2023-11-13 13:27:35 -0600

    better taglines

---
+++ src/analytics/clarity.jsx
@@ -0,0 +1,21 @@
+import Script from 'next/script';
+
+// Script for Microsoft Clarity Analytics
+const ClarityAnalytics = () => {
+  return (
+    <Script 
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function (c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "jq03ms730y");
+        `,
+      }}
+    />
+  );
+};
+
+export default ClarityAnalytics;
--- src/content/projects.js
+++ src/content/projects.js
@@ -16,7 +16,7 @@ const projects = [
   new Project({
     title: "Scribe",
     description: "Scribe is an AI-powered wep app that can create lecture notes and study guides in real-time from live lectures.",
-    tagline: "AI-powered study tool for students",
+    tagline: "Transforming live lectures into interactive notes and study guides",
     link: "https://notetaker-git-master-marcowang01.vercel.app/",
     github: "https://github.com/marcowang01/notetaker",
     date: "Fall, 2023",
@@ -27,7 +27,7 @@ const projects = [
   new Project({
     title: "Tomo",
     description: "Tomo is a real-time audio-based companion aimed at enhancing children’s learning through story‑driven dialogues.",
-    tagline: "Real-time audio-based companion for children",
+    tagline: "Bringing story-driven dialogues to life with story-driven dialogues",
     link: "https://tomo-git-mw-new-controls-yulilith.vercel.app/",
     github: "",
     date: "Summer, 2023",
@@ -38,7 +38,7 @@ const projects = [
   new Project({
     title: "Realm",
     description: "Realm (Retrieval Augmented Language Model) is an LLM pipeline that leverages vector databases to improve performance and reduce costs for question answering tasks.",
-    tagline: "Retrival Augmented LLM pipeline",
+    tagline: "Improving Q&A performance with vector database integration",
     link: "https://www.boboland.xyz/llm",
     github: "https://github.com/marcowang01/realm",
     date: "Spring, 2023",
@@ -48,8 +48,8 @@ const projects = [
   }),
   new Project({
     title: "Conway",
-    description: "Cuda Conway is a CUDA-accelerated implementation of Conway's Game of Life, a cellular automaton that simulates the evolution of a grid of cells based on a set of rules.",
-    tagline: "CUDA-accelerated Conway's Game of Life",
+    description: "Conway is a CUDA-accelerated implementation of Conway's Game of Life, a cellular automaton that simulates the evolution of a grid of cells based on a set of rules.",
+    tagline: "High-Speed Evolution with CUDA-Accelerated Cellular Automaton",
     link: "/files/conway.pdf",
     github: "https://github.com/marcowang01/conway-gpu",
     date: "Spring, 2023",
@@ -60,7 +60,7 @@ const projects = [
   new Project({
     title: "PlatzUI",
     description: "Platz is an open-source personal website template for creatives, with a focus on streamlining the design-to-implementation process.",
-    tagline: "Open-source personal website template",
+    tagline: "Open-source personal website template for creatives",
     link: "https://platz.ooo/",
     github: "https://github.com/szgbo/platz",
     date: "Winter, 2023",
@@ -71,7 +71,7 @@ const projects = [
   new Project({
     title: "Igloo",
     description: "Igloo is a reimagination of social tools, driven by organic interpersonal relationships. Currently, a prototype tested within a small community of 20+ users.",
-    tagline: "A reimagination of social tools",
+    tagline: "Reimagination of social connections in the digital age",
     link: "https://www.igloo.place/",
     github: "https://github.com/marcowang01/Igloo",
     date: "Summer, 2022",
@@ -82,7 +82,7 @@ const projects = [
   new Project({
     title: "Dash",
     description: "Dash is a full-stack telemetry dashboard built for the NU Formula Racing team to visualize, persist and analyze data from the car's sensors in real-time.",
-    tagline: "Telemetry dashboard for racing teams",
+    tagline: "Empowering FSAE Racing with real-time telemtry insights",
     link: "",
     github: "https://github.com/NU-Formula-Racing/telemetry-remote-23",
     date: "Winter, 2022",
@@ -93,7 +93,7 @@ const projects = [
   new Project({
     title: "Bounce Knight",
     description: "Bounce Knight is a 2D fighting game built in Unity. The game features a fully animated characters and 3 unique stages.",
-    tagline: "2D fighter built in Unity",
+    tagline: "Dynamic 2D fighter with animated characters",
     link: "",
     github: "https://github.com/marcowang01/Bounce-Knight-Unity2D/tree/master",
     date: "Winter, 2021",
