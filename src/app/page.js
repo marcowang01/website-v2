a919e17725104d87ecf95fec20b13043a7cef399
marcowang01 <marcowang01@gmail.com>
2023-11-12 23:39:39 -0600

    Add new features and fix bugs.

--- src/app/page.js
+++ src/app/page.js
@@ -10,13 +10,18 @@ export default function Home() {
 }
 
 // Todo:
-// add load in and exit animations with framer motion
+
 // archives page: tetris, conway, news analyzer, unity games, glide paper pipeline
 // create pages (vault) for conway, unity game, glide paper pipeline
-// for fun page: manga covers page, f1, squash? 
-// leave credit for Shaobo somewhere
 // add spotify link to the song
 // fix tomo page, make sure all github repos are presentatble
+
 // quick setup instructions and stuff, can use the github llm repo to generate a readme (easy readme)
-// add og images check on vercel
-// reroute the url
+
+// remake llm website on my own domain + conway slides
+// leave credit for Shaobo somewhere
+
+// add og images check on vercel (after design freeze)
+// add load in and exit animations with framer motion
+// for fun page: manga covers page, f1, squash? 
+
