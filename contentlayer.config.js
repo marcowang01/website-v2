302c427b5ca2f2aeac460805158ead15a859dbbc
marcowang01 <marcowang01@gmail.com>
2023-11-17 00:24:52 -0600

    markdown content baseline (no styling)

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,4 +1,4 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import { defineNestedType, defineDocumentType, makeSource } from 'contentlayer/source-files'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings' 
 import rehypePrettyCode from 'rehype-pretty-code'
 import rehypeSlug from 'rehype-slug'
@@ -15,6 +15,22 @@ const computedFields = {
     resolve: (doc) => doc._raw.flattenedPath.split('/').slice(1).join('/'),
   },
 }
+
+const Collaborators = defineNestedType(() => ({
+  name: 'Collaborators',
+  fields: {
+    names: {
+      type: 'list',
+      of: { type: 'string' },
+      required: true,
+    },
+    urls: {
+      type: 'list',
+      of: { type: 'string' },
+      required: true,
+    },
+  },
+}));
 
 export const Project = defineDocumentType(() => ({
   name: 'Project',
@@ -33,15 +49,18 @@ export const Project = defineDocumentType(() => ({
       type: 'string',
       required: true,
     },
-    contributors: {
-      type: 'list',
-      of: { type: 'string' },
-      required: true,
+    collaborators: {
+      type: 'nested',
+      of: Collaborators,
+      required: false,
     },
-    urls: {
-      type: 'list',
-      of: { type: 'string' },
-      required: true,
+    externalUrl: {
+      type: 'string',
+      required: false,
+    },
+    githubUrl: {
+      type: 'string',
+      required: false,
     },
   },
   computedFields,
--- src/app/collection/[slug]/page.js
+++ src/app/collection/[slug]/page.js
@@ -1,6 +1,8 @@
 import { allProjects } from 'contentlayer/generated'
 import { notFound } from 'next/navigation'
 import { Mdx } from '@/components/mdx/mdx'
+import Link from 'next/link'
+import styles from './page.module.css'
 
 async function getProjectFromParams(slug) {
   const project = allProjects.find((project) => project.slugAsParams === slug)
@@ -21,8 +23,19 @@ export default async function Page({params}) {
   const slug = params.slug
   const project = await getProjectFromParams(slug)
 
+  const { title, description, date, collaborators, githubUrl, externalUrl } = project
+
   return (
     <div>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.description}>{description}</div>
+      <div className={styles.date}>{date}</div>
+      <Link href={'/collection'} className={styles.back}>back to collection</Link>
+      {collaborators && collaborators.names.map((name, index) => (
+        <div key={`collaborator-${index}`} className={styles.collaborator}>
+          <Link href={collaborators.urls[index]} target="_blank" rel="noopener noreferrer">{name}</Link>
+        </div>
+      ))}
       <Mdx code={project.body.code} />
     </div>
   )
