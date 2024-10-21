e95c08f21006337b082557312c7694d49ef7d291
marcowang01 <marcowang01@gmail.com>
2024-10-21 02:01:35 -0700

    prettier

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,25 +5,41 @@ import ArrowIcon from '@/svg/arrow'
 
 
 function ContactLink({name, link, text}) {
-  return (
-    <div className={styles.linkContainer}>
-      <Link href={link} target="_blank" rel="noopener noreferrer" className={styles.link}>
+
+  const Content = () => (
+    <>
+      <div className={styles.link}>
         {name} 
         <ArrowIcon className={styles.arrow} />
-      </Link>
+      </div>
       <div className={styles.linkText}>
         {text}
       </div>
+      </>
+  )
+
+  if (link) {
+    return (
+      <Link href={link} target="_blank" rel="noopener noreferrer" className={styles.linkContainer}>
+        <Content />
+      </Link>
+    )
+  }
+
+  return (
+    <div className={styles.linkContainer}>
+    <Content />
     </div>
+    
   )
 }
 
 export default function Contact() {
   return (
     <main className={styles.main}>
-      <ContactLink name="Email" link="mailto:marcowang0101@gmail.com" text="marcowang0101@gmail.com" />
       <ContactLink name="LinkedIn" link="https://www.linkedin.com/in/marco-01/" text="@marco-01" />
       <ContactLink name="Github" link="https://github.com/marcowang01" text="@marcowang01" />
+      <ContactLink name="Email" link="" text="<first><last>0101@gmail.com" />
     </main>
   )
 }
