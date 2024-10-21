4819ea05e839a4b5d3f9134da45e1dc3028bee9c
marcowang01 <marcowang01@gmail.com>
2024-10-21 02:52:20 -0700

    add more random things

--- src/app/random/page.js
+++ src/app/random/page.js
@@ -5,19 +5,21 @@ export default function Random() {
     <main className={styles.main}>
       <section className={styles.section}>
         <h3 className={styles.sectionTitle}>
-          Reads
+          Reads/Reading/Read
         </h3>
         <ul className={styles.list}>
           <li className={styles.listItem}>Vagabond</li>
-          <li className={styles.listItem}>神鵰俠侶</li>
+          <li className={styles.listItem}>神鵰俠侶 (Return of the Condor Heroes)</li>
           <li className={styles.listItem}>Alice in Borderland</li>
           <li className={styles.listItem}>The Three Body Problem: Dark Forest</li>
           <li className={styles.listItem}>Chainsaw Man</li>
+          <li className={styles.listItem}>Character of Physical Law</li>
+          <li className={styles.listItem}>Almost Buddhist</li>
         </ul>
       </section>
       <section className={styles.section}>
         <h3 className={styles.sectionTitle}>
-          Watches
+          Watches/Watching/Watched
         </h3>
         <ul className={styles.list}>
           <li className={styles.listItem}>Ping Pong The Animation</li>
@@ -25,23 +27,24 @@ export default function Random() {
           <li className={styles.listItem}>Cowboy Bebop</li>
           <li className={styles.listItem}>The Three Idiots</li>
           <li className={styles.listItem}>Interstellar</li>
-          <li className={styles.listItem}>The God of Gamblers</li>
+          <li className={styles.listItem}>The God of Gamblers II</li>
+          <li className={styles.listItem}>The Boy and the Heron</li>
         </ul>
       </section>
       <section className={styles.section}>
         <h3 className={styles.sectionTitle}>
-          Plays
+          Plays/Playing/Played
         </h3>
         <ul className={styles.list}>
           <li className={styles.listItem}>Badminton</li>
           <li className={styles.listItem}>Squash</li>
           <li className={styles.listItem}>Golf</li>
-          <li className={styles.listItem}>Bouldering</li>
+          <li className={styles.listItem}>It Takes Two</li>
           <li className={styles.listItem}>Overcooked 2</li>
           <li className={styles.listItem}>Teamfight Tactics</li>
           <li className={styles.listItem}>Hearthstone</li>
-          <li className={styles.listItem}>掼蛋</li>
-          <li className={styles.listItem}>拖拉机</li>
+          <li className={styles.listItem}>掼蛋 (Guandan)</li>
+          <li className={styles.listItem}>拖拉机 (Tractor Upgrades)</li>
         </ul>
       </section>
     </main>
