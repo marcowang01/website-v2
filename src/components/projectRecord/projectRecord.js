72cf9f4ed0d379aeb98938ade2f213f914f16b75
marcowang01 <marcowang01@gmail.com>
2025-02-04 23:30:36 -0600

    lint

--- src/components/projectRecord/projectRecord.js
+++ src/components/projectRecord/projectRecord.js
@@ -4,36 +4,27 @@ import styles from './projectRecord.module.css'
 import Link from 'next/link'
 
 export default function ProjectRecord(project) {
-  const {title, tagline, link, github, date, category, starred} = project
+  const { title, tagline, link, github, date, category, starred } = project
 
   const priorityLink = link || github
 
-  // const handleOnClick = (e) => {
-  //   e.preventDefault();
-  //   e.stopPropagation();
-  //   console.log(priorityLink)
-  //   window.open(priorityLink, '_blank', 'noopener,noreferrer');
-  // }
-
   return (
-    <Link href={priorityLink} target="_blank" rel="noopener noreferrer" className={styles.container}>
+    <Link
+      href={priorityLink}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={styles.container}
+    >
       <section className={styles.headerContainer}>
         <div className={styles.title}>
           {title}
           {/* <ArrowIcon /> */}
         </div>
-        <div className={styles.date}>
-          {date}
-        </div>
-
+        <div className={styles.date}>{date}</div>
       </section>
       <section className={styles.footerContainer}>
-        <div className={styles.tagline}>
-          {tagline}
-        </div>
-        <div className={styles.category}>
-          {category}
-        </div>
+        <div className={styles.tagline}>{tagline}</div>
+        <div className={styles.category}>{category}</div>
       </section>
     </Link>
   )
