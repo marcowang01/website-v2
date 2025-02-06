7428739bc7fe8f9b0981ad9356cdd148ed52e5cb
marcowang01 <marcowang01@gmail.com>
2025-02-05 20:01:11 -0600

    fix ts tailwind

--- src/components/projectRecord/projectRecord.tsx
+++ src/components/projectRecord/projectRecord.tsx
@@ -1,5 +1,4 @@
 'use client'
-import styles from './projectRecord.module.css'
 import Link from 'next/link'
 import { Project } from '@/content/projects'
 
@@ -11,15 +10,24 @@ export function ProjectRecord({ project }: { project: Project }) {
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className={styles['container']}
+      className="flex flex-col justify-between items-start w-[87%] p-3 rounded-lg text-[color:rgb(var(--project-card-text-rgb))] cursor-pointer transition-all duration-250 origin-center hover:bg-[color:rgb(var(--project-card-hover-bg-rgb))] active:scale-99 md:min-w-[unset]"
     >
-      <section className={styles['headerContainer']}>
-        <div className={styles['title']}>{title}</div>
-        <div className={styles['date']}>{date}</div>
+      <section className="flex flex-row justify-between items-center gap-2.5 w-full">
+        <div className="flex flex-row items-center gap-2 font-medium text-base tracking-tight transition-all duration-200">
+          {title}
+        </div>
+        <div className="font-light text-xs tracking-tight uppercase text-[color:rgb(var(--project-card-light-text-rgb))]">
+          {date}
+        </div>
       </section>
-      <section className={styles['footerContainer']}>
-        <div className={styles['tagline']}>{tagline}</div>
-        <div className={styles['category']}>{category}</div>
+
+      <section className="flex flex-row justify-between items-center w-full md:justify-start">
+        <div className="w-fit text-sm tracking-tight leading-tight">
+          {tagline}
+        </div>
+        <div className="w-fit text-sm tracking-tight uppercase text-[color:rgb(var(--project-card-light-text-rgb))] md:hidden">
+          {category}
+        </div>
       </section>
     </Link>
   )
