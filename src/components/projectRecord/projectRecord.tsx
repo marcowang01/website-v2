fab986c01bcd3cb37bf9481fa6c6e6f2a59b06f2
marcowang01 <marcowang01@gmail.com>
2025-02-06 21:42:12 -0600

    lint

--- src/components/projectRecord/projectRecord.tsx
+++ src/components/projectRecord/projectRecord.tsx
@@ -35,8 +35,8 @@ export function ProjectRecord({
   return (
     <div
       className={cn(
-        'flex flex-start items-start gap-[10px]',
-        'min-w-[calc(100vw-22px-2rem)] mr-[calc(11px+1rem)]',
+        'flex-start flex items-start gap-[10px]',
+        'mr-[calc(11px+1rem)] min-w-[calc(100vw-22px-2rem)]',
         'px-[10px] py-[8px]',
         'rounded-[10px]',
         'text-[color:rgb(var(--project-card-text-rgb))]',
@@ -44,7 +44,7 @@ export function ProjectRecord({
         'transition-all duration-300 ease-in-out',
         'origin-center',
         'group',
-        'md:w-[87%] md:min-w-0 md:mr-0',
+        'md:mr-0 md:w-[87%] md:min-w-0',
         'md:px-[12px] md:py-[10px]',
         'md:hover:bg-[color:rgb(var(--project-card-hover-bg-rgb))]',
         {
@@ -53,27 +53,27 @@ export function ProjectRecord({
       )}
       onClick={onClick}
     >
-      <div className="flex flex-col justify-between items-start">
-        <section className="flex flex-row justify-between items-center gap-[10px] w-full">
-          <div className="flex flex-row items-baseline gap-3 font-medium text-base tracking-[-0.005em] leading-[1.25]">
+      <div className="flex flex-col items-start justify-between">
+        <section className="flex w-full flex-row items-center justify-between gap-[10px]">
+          <div className="flex flex-row items-baseline gap-3 text-base font-medium leading-[1.25] tracking-[-0.005em]">
             {title}
-            <div className="w-fit text-[13px] tracking-[-0.02em] font-normal uppercase text-[color:rgb(var(--project-card-light-text-rgb))] hidden md:block">
+            <div className="hidden w-fit text-[13px] font-normal uppercase tracking-[-0.02em] text-[color:rgb(var(--project-card-light-text-rgb))] md:block">
               {category}
             </div>
           </div>
-          <div className="font-light text-xs tracking-[-0.02em] uppercase text-[color:rgb(var(--project-card-light-text-rgb))]">
+          <div className="text-xs font-light uppercase tracking-[-0.02em] text-[color:rgb(var(--project-card-light-text-rgb))]">
             {date}
           </div>
         </section>
 
-        <section className="flex flex-row justify-start md:justify-between items-center w-full">
-          <div className="w-fit text-sm tracking-[-0.02em] leading-[1.2]">
+        <section className="flex w-full flex-row items-center justify-start md:justify-between">
+          <div className="w-fit text-sm leading-[1.2] tracking-[-0.02em]">
             {tagline}
           </div>
           <div className="text-[color:rgb(var(--project-card-light-text-rgb))]">
             <CaretDownIcon
               className={cn(
-                'transform transition-transform duration-500 ease-in-out shrink-0',
+                'shrink-0 transform transition-transform duration-500 ease-in-out',
                 {
                   'rotate-180': isExpanded,
                 }
@@ -90,7 +90,7 @@ export function ProjectRecord({
             scrollbarGutter: 'auto',
           }}
           className={cn(
-            'transition-all duration-300 ease-in-out overflow-hidden w-full',
+            'w-full overflow-hidden transition-all duration-300 ease-in-out',
             {
               'opacity-100': isExpanded,
               'opacity-0': !isExpanded,
@@ -98,8 +98,34 @@ export function ProjectRecord({
           )}
         >
           <div className="flex flex-row justify-between gap-[10px] pt-4">
-            <div className="flex flex-col gap-[10px] w-[50%]">
-              <p className="text-sm leading-[1.2]">
+            <div className="flex w-[50%] flex-col gap-[10px]">
+              <div className="flex flex-row items-baseline justify-start gap-4 pt-2 text-[color:rgb(var(--project-card-light-text-rgb))]">
+                <div className="transition-color flex flex-row items-baseline justify-start gap-1 duration-300 ease-in-out hover:text-[color:rgb(var(--project-card-text-rgb))]">
+                  <Link
+                    href={link}
+                    className="min-w-content text-xs leading-[1.2] tracking-[-0.02em]"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {link.endsWith('.pdf') ? 'PAPER' : 'WEBSITE'}
+                  </Link>
+                  <ArrowIcon className="h-[9px] w-[9px]" />
+                </div>
+                {github && (
+                  <div className="transition-color flex flex-row items-baseline justify-start gap-1 duration-300 ease-in-out hover:text-[color:rgb(var(--project-card-text-rgb))]">
+                    <Link
+                      href={github}
+                      className="min-w-content text-xs leading-[1.2] tracking-[-0.02em]"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      GITHUB
+                    </Link>
+                    <ArrowIcon className="h-[9px] w-[9px]" />
+                  </div>
+                )}
+              </div>
+              <p className="pl-2 text-sm leading-[1.2]">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Tempore recusandae odit quo quas corrupti facilis animi ut, et
                 laborum iste provident non accusantium optio fuga repellat amet,
@@ -108,40 +134,13 @@ export function ProjectRecord({
                 corrupti facilis animi ut, et laborum iste provident non
                 accusantium optio fuga repellat amet, velit dolores asperiores.
               </p>
-              {/* links to website and github */}
-              <div className="flex flex-row items-baseline gap-4 justify-start pt-2 text-[color:rgb(var(--project-card-light-text-rgb))]">
-                <div className="flex flex-row items-baseline gap-1 justify-start hover:text-[color:rgb(var(--project-card-text-rgb))] transition-color duration-300 ease-in-out">
-                  <Link
-                    href={link}
-                    className="text-xs tracking-[-0.02em] leading-[1.2] min-w-content"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {link.endsWith('.pdf') ? 'PAPER' : 'WEBSITE'}
-                  </Link>
-                  <ArrowIcon className="w-[9px] h-[9px]" />
-                </div>
-                {github && (
-                  <div className="flex flex-row items-baseline gap-1 justify-start hover:text-[color:rgb(var(--project-card-text-rgb))] transition-color duration-300 ease-in-out">
-                    <Link
-                      href={github}
-                      className="text-xs tracking-[-0.02em] leading-[1.2] min-w-content"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      GITHUB
-                    </Link>
-                    <ArrowIcon className="w-[9px] h-[9px]" />
-                  </div>
-                )}
-              </div>
             </div>
             <Image
               src={'https://placehold.co/200x100.png'}
               alt={'project iamge'}
               width={200}
               height={100}
-              className="rounded-md object-cover shrink-0"
+              className="shrink-0 rounded-md object-cover"
             />
           </div>
         </section>
