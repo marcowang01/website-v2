83b1e35aabb1ed0c1f8c57f1c4f3bd4eb32cdd3b
marcowang01 <marcowang01@gmail.com>
2025-02-19 17:52:17 -0800

    update description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ const inter = Inter({
 export const metadata = {
   title: 'Marco Wang',
   description:
-    'Software Engineer. Building AI agents at rabbit. Creating tools to help people learn, create and connect.',
+    'Software Engineer. Building AI agents at rabbit. Creating tools to help people learn, create and connect. Physics/Computer Science at Northwestern University.',
   keywords: ['Marco Wang', 'Software', 'Engineer', 'Personal Website'],
   authors: [{ name: 'Marco Wang' }],
   creator: 'Marco Wang',
