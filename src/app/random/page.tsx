8d90eff3956cb6a006911fc771627f2db76ebf0a
marcowang01 <marcowang01@gmail.com>
2025-02-24 23:35:19 -0600

    random fix

--- src/app/random/page.tsx
+++ src/app/random/page.tsx
@@ -3,42 +3,20 @@ import styles from './page.module.css'
 export default function Random() {
   const sections = [
     {
-      title: 'Reads/Reading/Read',
-      items: [
-        'Vagabond',
-        '神鵰俠侶 (Return of the Condor Heroes)',
-        'Alice in Borderland',
-        'The Three Body Problem: Dark Forest',
-        'Character of Physical Law',
-        'Almost Buddhist',
-      ],
-    },
-    {
-      title: 'Watches/Watching/Watched',
+      title: 'Recently Watched',
       items: [
         'Ping Pong The Animation',
         'Cowboy Bebop',
-        "Frieren: Beyond Journey's End",
-        'The Three Idiots',
-        'Interstellar',
-        'The God of Gamblers II',
-        'The Boy and the Heron',
         'Arcane',
+        'The Boy and the Heron',
       ],
     },
     {
-      title: 'Plays/Playing/Played',
+      title: 'Enjoys',
       items: [
-        'Badminton',
-        'Squash',
-        'Ski',
-        'Golf',
-        'It Takes Two',
-        'Overcooked 2',
-        'Teamfight Tactics',
-        'Hearthstone',
-        '掼蛋 (Guandan)',
-        '拖拉机 (Tractor Upgrades)',
+        'Badminton Doubles',
+        'Skiing on fresh powder',
+        'Getting three stars in Overcooked 2',
       ],
     },
   ]
@@ -47,10 +25,10 @@ export default function Random() {
     <main className={styles['main']}>
       {sections.map((section, index) => (
         <section key={index} className={styles['section']}>
-          <h3 className={styles['section-title']}>{section.title}</h3>
+          <h3 className={styles['sectionTitle']}>{section.title}</h3>
           <ul className={styles['list']}>
             {section.items.map((item, itemIndex) => (
-              <li key={itemIndex} className={styles['list-item']}>
+              <li key={itemIndex} className={styles['listItem']}>
                 {item}
               </li>
             ))}
